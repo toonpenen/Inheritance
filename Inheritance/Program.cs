50851859eb8cd93696666511f2175fa96d1693df
toonpenen <Toon.Penen@student.intecbrussel.be>
2020-11-19 11:10:11 +0100

    Add project files.

---
+++ Inheritance/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    public class Class1
+    {
+    public string Name { get; set; }
+
+    public void Show1()
+    {
+        Console.WriteLine("I'm class1 method show1");
+    }
+    public void Show2()
+    {
+        Console.WriteLine("I'm class2 method show2");
+    }
+}
+    public class Class2:Class1
+    {
+        public int Age { get; set; }
+        
+        public Class2() 
+        {
+            Console.WriteLine("I'm class2 constructor");
+        }
+        public void Show3()
+        {
+            Console.WriteLine("I'm class1 method show3"); ;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class2 c = new Class2();
+            c.Show1();
+            c.Show2();
+            c.Show3();
+           
+
+            Class1 p = new Class1();
+            
+        }
+    }
+}
